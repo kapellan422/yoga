0a806fe0b7a8693fade8991d3672c5ca6183afc0
Mikhail <kapellan422@gmail.com>
2022-11-30 12:29:44 +0600

    add timer scripts

--- js/script.js
+++ js/script.js
@@ -27,10 +27,69 @@ window.addEventListener('DOMContentLoaded', function(){
             for(let i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
                     hideTabContent(0);
-                    showTabContent(i);
-                    break;
+                    showTabContent(i); 
+                    break; 
                 }
             }
         }
     });
+
+    // Timer
+
+    let deadline = '2023-12-01';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime){
+        let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            if(t.hours <= 9) {
+                hours.textContent = '0' + t.hours;
+            } 
+            else {
+                hours.textContent = t.hours;
+            }
+
+            if(t.minutes <= 9) {
+                minutes.textContent = '0' + t.minutes;
+            } 
+            else {
+                minutes.textContent = t.minutes;
+            }
+
+            if(t.seconds <= 9) {
+                seconds.textContent = '0' + t.seconds;
+            } 
+            else {
+                seconds.textContent = t.seconds;
+            }
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = ('00');
+                minutes.textContent = ('00');
+                seconds.textContent = ('00');
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
