d426bfa8df7eb113d1916593a8dc3d566e9d4fa5
Mikhail <kapellan422@gmail.com>
2022-12-13 12:45:50 +0600

    add class Options

--- js/script.js
+++ js/script.js
@@ -121,4 +121,17 @@ window.addEventListener('DOMContentLoaded', function(){
         });
     }
 
+    
+    //Class Options
+
+    class Options {
+        constructor(height, width, bg, fontSize, textAlign) {
+            this.height = height;
+            this.width = width;
+            this.bg = bg;
+            this.fontSize = fontSize;
+            this.textAlign = textAlign;
+        }
+
+    }
 });
